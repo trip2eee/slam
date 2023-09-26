44bdece64b6a4dfa856d1562d4e5690dea8a00e8
jongmin.park <trip2eee@gmail.com>
2023-09-26 23:58:00 +0900

    improve particle filter

--- 04_nonparametric_filters/particle_filter.py
+++ 04_nonparametric_filters/particle_filter.py
@@ -34,6 +34,27 @@ class ParticleFilter:
 
             self.X.append(x)
 
+    def sample_uniform(self):
+        Xt = []
+        for m in range(self.M):
+            x = np.random.randint(low=0, high=x_max)
+            Xt.append(x)
+        self.X = Xt
+
+    def sample_gaussian(self):
+        Xt = []
+        
+        for m in range(self.M):
+            std_x = 0.5
+            mean_x = self.X[m]
+
+            x = np.random.randn()*std_x + mean_x
+            # x = np.clip(x, a_min=0, a_max=x_max-1)
+            # x = int(x + 0.5)
+
+            Xt.append(x)
+        self.X = Xt
+
     def resample(self):
         """ Low variance sampler (Table 4.4, p86)
         """
@@ -67,16 +88,13 @@ class ParticleFilter:
             # xm, wm : position and importance of the m-th sample
             xm = self.X[m]
             wm  = self.w[m]
+            xm = np.clip(xm, a_min=0, a_max=x_max-1)
+            xm = int(xm + 0.5)            
             self.px[xm] += wm
-
-            if xm < x_max - 1:
-                self.px[xm + 1] += wm*0.2
-            if 0 < xm:
-                self.px[xm - 1] += wm*0.2
 
         self.px = self.px / np.sum(self.px)
 
-x_max = 100
+x_max = 50
 x_obj = 30
 x_robot = 0
 
@@ -86,15 +104,23 @@ fx_obj = normal_dist(x_obj, 2.5, x)
 fx_obj = np.ones([x_max])
 fx_obj = fx_obj / np.sum(fx_obj)
 
-M = 10
+M = 50
 filter = ParticleFilter(M, fx_obj)
 
 for i in range(20):
     print('iteration: ', i)
 
     plt.figure()
-    filter.sample()
-    print('  ', filter.X)
+    # filter.sample()
+    if i == 0:
+        filter.sample_uniform()
+    else:
+        filter.sample_gaussian()
+
+    for x in filter.X:
+        print('{:.2f}, '.format(x), end='')
+    print('')
+    
     # draw samples
     plt.subplot(311)
     zeros = [0]*filter.M
@@ -103,24 +129,29 @@ for i in range(20):
     plt.scatter(x_obj, 0, c='r', label='object')
     plt.scatter(x_robot, 0, c='b', label='robot')    
     plt.scatter(filter.X, zeros, c='m', marker='x', label='samples')
+    plt.legend()
+
     plt.title('samples')
     
     # compute pdf f(z)
     pz = np.zeros([x_max])
     for m in range(M):
         z = filter.X[m]
+        z = np.clip(z, a_min=0, a_max=x_max-1)
+        z = int(z+0.5)
         pz[z] += 1
-
     pz /= M
 
     for m in range(M):
         z = filter.X[m]
+        z = np.clip(z, a_min=0, a_max=x_max-1)
+        z = int(z+0.5)
         
         # compute importance factor w_m = p(z | x_m)
         # p(z | x_m) = p(x_m | z) * p(z) / p(x_m)        
         pxz = normal_dist(x_obj, 1, z)
-        pzm = pxz * pz[z] / filter.px[z]
-                
+        pzm = pxz * pz[z] / (filter.px[z] + 1e-6)
+
         filter.w[m] = pzm
 
     plt.subplot(312)
@@ -135,7 +166,6 @@ for i in range(20):
     plt.plot(filter.px)
     plt.title('p(x)')
 
-    plt.legend()
     plt.show()
 
 

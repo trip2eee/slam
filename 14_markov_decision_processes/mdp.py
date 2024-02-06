9be387938ee55f097b2a4e13677c037857d7ccb1
jongmin.park <trip2eee@gmail.com>
2024-02-06 23:37:24 +0900

    [MDP] Map for MDP

---
+++ 14_markov_decision_processes/mdp.py
@@ -0,0 +1,14 @@
+""" Markov Decision Processes
+    MDP value iteration and policy (Table 14.1 on page 502)
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    map = cv2.imread('mdp_map.png')
+    plt.imshow(map)
+
+    plt.show()
+

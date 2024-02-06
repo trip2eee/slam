[EKF SLAM] EKF SLAM improvement - Bug fix in feature map covariance visualization - Trajectory visualization is added. - Noise covariances are tuned.

10_simultaneous_localization_and_mapping/ekf_slam1.py
@@ -18,6 +18,13 @@ landmarks = [
 landmarks = np.array(landmarks, dtype=np.float32)
 
 INFINITE = 5**2
+r_max = 6.0     # max detection range
+
+STD_V = 0.3
+STD_W = 1 * np.pi / 180
+
+STD_R = 0.1
+STD_PHI = 0.001
 
 # The number of landmarks (measurements)
 M = landmarks.shape[0]
@@ -36,27 +43,30 @@ class EKF_SLAM:
         self.P[0,0] = 1.0
         self.P[1,1] = 1.0
         self.P[2,2] = 1.0
-        
 
         self.x_pred = self.x.copy()
         self.P_pred = self.P.copy()
-        self.N = 0
+        self.N = 0  # the number of map features
+
+        self.list_x_gt = []
+        self.list_x_est = []
 
     def predict(self, ut, dt=0.1):
         vt, wt = ut
 
-        theta = self.x[2,0]
-
+        # estimate the ground truth path
+        theta = self.x_gt[2,0]
         dx = np.array([
             [-vt/wt*np.sin(theta) + vt/wt*np.sin(theta + wt*dt)],
             [ vt/wt*np.cos(theta) - vt/wt*np.cos(theta + wt*dt)],
             [wt*dt],
         ])
         self.x_gt[:3] += dx # update ground truth
+        self.list_x_gt.append(self.x_gt.copy())
 
-
-        vt += np.random.randn()*0.3
-        wt += np.random.randn()*0.3
+        theta = self.x[2,0]
+        vt += np.random.randn()*STD_V
+        wt += np.random.randn()*STD_W
         dx = np.array([
             [-vt/wt*np.sin(theta) + vt/wt*np.sin(theta + wt*dt)],
             [ vt/wt*np.cos(theta) - vt/wt*np.cos(theta + wt*dt)],
@@ -76,10 +86,12 @@ class EKF_SLAM:
         ])
         Gt = np.eye(3+3*(M+1), 3+3*(M+1)) + np.matmul(np.matmul(Fx.T, g), Fx)
 
+        std_v = STD_V*dt
+        std_w = STD_W*dt
         Rt = np.array([
-            [0.1**2, 0, 0],
-            [0, 0.1**2, 0],
-            [0, 0, 0.01**2]
+            [std_v**2, 0, 0],
+            [0, std_v**2, 0],
+            [0, 0, std_w**2]
         ], dtype=np.float32)
 
         self.P_pred = np.matmul(np.matmul(Gt, self.P), Gt.T) + np.matmul(np.matmul(Fx.T, Rt), Fx)
@@ -96,10 +108,16 @@ class EKF_SLAM:
         phi = np.arctan2(dy, dx) - theta
         s = ms
 
-        r += np.random.randn()*0.1
-        phi += np.random.randn()*0.001
+        # phi shall be [-pi, pi]
+        if phi > np.pi:
+            phi -= 2*np.pi
+        elif phi < -np.pi:
+            phi += 2*np.pi
 
-        if r <= 6:
+        if r <= r_max:            
+            r += np.random.randn()*STD_R
+            phi += np.random.randn()*STD_PHI
+
             return np.array([[r, phi, s]], dtype=np.float32).T
         else:
             return None
@@ -109,7 +127,7 @@ class EKF_SLAM:
         x_pred = self.x_pred
         P_pred = self.P_pred
 
-        s_r = 1.0
+        s_r = 0.5
         s_phi = 0.1
         s_s = 0.1
         Q = np.array([
@@ -175,6 +193,12 @@ class EKF_SLAM:
 
                 r = zi_meas-zi_pred                                
 
+                # phi shall be [-pi, pi]
+                if r[1,0] > np.pi:
+                    r[1,0] -= 2*np.pi
+                elif r[1,0] < -np.pi:
+                    r[1,0] += 2*np.pi
+
                 if k == N:
                     d = alpha   # line 19 gives the maximum distance to N+1
                 else:
@@ -194,26 +218,57 @@ class EKF_SLAM:
             x_pred = x_pred + np.matmul(Kt, min_y)
             P_pred = np.matmul((np.eye(3+3*(M+1), 3+3*(M+1)) - np.matmul(Kt, min_H)), P_pred)
 
+        self.x_pred = x_pred
         self.x = x_pred
         self.P = P_pred
 
+        self.list_x_est.append(self.x.copy())
+
     def plot(self):
-        
+        plt.figure('map')
+        plt.clf()
+
+        # draw path
+        list_x_gt = np.array(self.list_x_gt)
+        list_x_est = np.array(self.list_x_est)
+        plt.plot(list_x_gt[:,0], list_x_gt[:,1], c='g')
+        plt.plot(list_x_est[:,0], list_x_est[:,1], c='b')
+
         # draw landmarks
         plt.scatter(landmarks[:,0], landmarks[:,1], c='k')
-        for i in range(M):
+
+        for i in range(self.N):
             j = 3+i*3
 
-            if self.P[j,j] < INFINITE:
-                self.draw_cov_ellipse(self.x[j:j+3], self.P[j:j+3,j:j+3], color='r')
-                plt.scatter(self.x[j], self.x[j+1], color='r')
+            xj = self.x_pred[j:j+3]
+            Pj = self.P[j:j+3,j:j+3]
+
+            dx = self.x_pred[j+0,0] - self.x_pred[0,0]
+            dy = self.x_pred[j+1,0] - self.x_pred[1,0]
+            q = dx**2 + dy**2
+
+            # dr/dx,   dr/dy,   dr/dtheta
+            # dphi/dx, dphi/dy, dphi/dtheta
+            Hj = 1/q*np.array([
+                [-np.sqrt(q)*dx, -np.sqrt(q)*dy,  0],
+                [            dy,            -dx, -q],
+                [             0,              0,  0],
+            ])
+
+            Pmj = np.matmul(np.matmul(Hj, Pj), Hj.T)
+            self.draw_cov_ellipse(xj, Pmj, color='c')
+            plt.scatter(self.x[j], self.x[j+1], color='r')
 
         # draw robot
         self.draw_robot(self.x_gt, color='g')
         self.draw_robot(self.x_pred, color='c')
         self.draw_robot(self.x, color='b')
-        
+
         self.draw_cov_ellipse(self.x, self.P[:2,:2], color='b')
+
+        plt.axis('equal')
+        plt.draw()
+        plt.waitforbuttonpress(0.1)
 
     def draw_robot(self, x, color):
         x_robot = x[0,0]
@@ -253,40 +308,35 @@ class EKF_SLAM:
         
         plt.plot(xs, ys, c=color)
 
-ekf_slam = EKF_SLAM()
+if __name__ == '__main__':
 
-tm = 3  # time multiplier
+    ekf_slam = EKF_SLAM()
+    tm = 2  # time multiplier
 
-for t in range(21*tm):
-    fig = plt.figure('map')
+    for t in range(21*tm):
+        
+        print('time:',t)
 
-    print('time:',t)
+        # Robot maneuver
+        if t <= 7*tm:
+            ut = [25, 0.001]
+        elif t <= 9*tm:
+            ut = [25, -np.pi/2*5]
+        elif t <= 10*tm:
+            ut = [25, 0.001]
+        elif t <= 12*tm:
+            ut = [25, -np.pi/2*5]
+        elif t <= 16*tm:
+            ut = [25, 0.001]
+        elif t <= 17*tm:
+            ut = [25, -np.pi/2*5]
+        else:
+            ut = [25, 0.001]
 
-    # Robot maneuver
-    if t <= 7*tm:
-        ut = [25, 0.001]
-    elif t <= 9*tm:
-        ut = [25, -np.pi/2*5]
-    elif t <= 10*tm:
-        ut = [25, 0.001]
-    elif t <= 12*tm:
-        ut = [25, -np.pi/2*5]
-    elif t <= 16*tm:
-        ut = [25, 0.001]
-    elif t <= 17*tm:
-        ut = [25, -np.pi/2*5]
-    else:
-        ut = [25, 0.001]
+        ekf_slam.predict(ut, dt=0.1/tm)
+        ekf_slam.update()
 
-    ekf_slam.predict(ut, dt=0.1/tm)
-    ekf_slam.update()
+        ekf_slam.plot()
 
-    ekf_slam.plot()
-
-    plt.draw()
-    plt.waitforbuttonpress(0)
-    plt.close(fig)
-
-
-
+    plt.show()
 